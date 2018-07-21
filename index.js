b5772103f78fad8f2472603cbdf6a28870dbfd60
Mikhail Privalov <n5250@ya.ru>
2018-07-22 02:14:29 +0800

    New functions and JSDoc fixes

--- index.js
+++ index.js
@@ -1,60 +1,111 @@
-;(function(root, factory){
+; (function (root, factory) {
 
-    if( typeof module !== 'undefined' && module.exports){
+    if (typeof module !== 'undefined' && module.exports) {
         module.exports = factory();
-    }else{
+    } else {
         root.hashrate = factory();
     }
 
 })(this, function () {
-
-    /*
-        @ hashrate <String> or <Number>
-        count of shares
-    */
+    /**
+     * @param {number} hashrate
+     * @returns Hashrate in string
+     */
     function hashrateToString(hashrate) {
-        let i = -1;
-        const byteUnits = ['KH', 'MH', 'GH', 'TH', 'PH'];
-
-            do {
-                hashrate = hashrate / 1000;
-                i++;
-            } while (hashrate >= 1000);
-
-        return hashrate.toFixed(2) + byteUnits[i];
+        return hashratePowerStr(hashrate, getPower(hashrate));
     };
 
     /**
-     * @ difficulty <Number>
-     * @ hashrate <Number>
-     * return time of block create <ms>
+     * @param {number} difficulty
+     * @param {number} hashrate
+     * @returns {number} time of block creation
     */
     function getEstimatedMinigTime(difficulty, hashrate) {
         return difficulty * 2 ** 32 / hashrate;
     }
 
-    /*
-    @ diff <Number> 
-    @ hashrate <Number>
-    */
+    /**
+     * @param {number} diff
+     * @param {number} hashrate
+     * @param {number} period
+     * @param {number} [blocks=1]
+     * @returns {number} luck
+     */
     function getLuck(diff, hashrate, period, blocks) {
         return getEstimatedMinigTime(diff * (blocks || 1), hashrate) / period || 0;
     };
 
+    /**
+     * Returns power of hashrate value.
+     * Ex.:
+     * hashrate < 1000 => power = 0
+     * hashrate >= 1000 && < 1000000 => power = 1
+     * hashrate >= 1000000 && < 1000000000 => power = 2
+     * @param {number} hashrate
+     * @returns {number} Power value for hashrate
+     */
     function getPower(hashrate) {
-        let i = 0;
-        while(hashrate >= 1000) {
+        var i = 0;
+        while (hashrate >= 1000) {
             hashrate = hashrate / 1000;
             i++;
         }
         return i;
     };
-    
+
+    /**
+     * @param {number[]} hashrates array of hashrates
+     * @returns {number} Max power of hashrates
+     */
+    function getMaxPower(hashrates) {
+        var i = 0;
+        for (var j = 0; j < hashrates.length; j++) {
+            i = Math.max(i, getPower(hashrates[j]));
+        }
+        return i;
+    };
+
+    /**
+     * Returns power in string.
+     * Ex.:
+     * power === 0 => H
+     * power === 1 => KH
+     * power === 2 => MH
+     * @param {number} power Power value
+     * @returns {string} Power in string
+     */
+    function getPowerStr(power) {
+        return ['H', 'KH', 'MH', 'GH', 'TH', 'PH'][power];
+    };
+
+    /**
+     * @param {number} hashrate
+     * @param {number} power
+     * @returns {number} The hashrate given to the power
+     */
+    function hashrateValueToPower(hashrate, power) {
+        return hashrate / Math.pow(1000, power);
+    };
+
+    /**
+     * 
+     * @param {number} hashrate
+     * @param {number} power
+     * @returns {string} Combination of hashrateValueToPower and getPowerStr
+     */
+    function hashratePowerStr(hashrate, power) {
+        return hashrateValueToPower(hashrate, power).toFixed(2) + ' ' + getPowerStr(power);
+    }
+
 
     return {
         hashrateToString: hashrateToString,
         getEstimatedMinigTime: getEstimatedMinigTime,
         getLuck: getLuck,
         getPower: getPower,
+        getMaxPower: getMaxPower,
+        getPowerStr: getPowerStr,
+        hashrateValueToPower: hashrateValueToPower,
+        hashratePowerStr: hashratePowerStr,
     };
 })
--- tests/simple-test.js
+++ tests/simple-test.js
@@ -3,11 +3,11 @@ const hashrate = require('../index');
 describe('Hashrate', () => {
 
     it('hashrateToString', ()=> {
-        expect(hashrate.hashrateToString(1000000000000000)).toEqual('1.00PH');
-        expect(hashrate.hashrateToString(1000000000000)).toEqual('1.00TH');
-        expect(hashrate.hashrateToString(1000000000)).toEqual('1.00GH');
-        expect(hashrate.hashrateToString(1000000)).toEqual('1.00MH');
-        expect(hashrate.hashrateToString(1200)).toEqual('1.20KH');
+        expect(hashrate.hashrateToString(1000000000000000)).toEqual('1.00 PH');
+        expect(hashrate.hashrateToString(1000000000000)).toEqual('1.00 TH');
+        expect(hashrate.hashrateToString(1000000000)).toEqual('1.00 GH');
+        expect(hashrate.hashrateToString(1000000)).toEqual('1.00 MH');
+        expect(hashrate.hashrateToString(1200)).toEqual('1.20 KH');
     });
 
     it('getEstimatedMinigTime', ()=> {
@@ -23,5 +23,25 @@ describe('Hashrate', () => {
         expect(hashrate.getPower(1000)).toEqual(1);
         expect(hashrate.getPower(10000000)).toEqual(2);
         expect(hashrate.getPower(10000000000)).toEqual(3);
+    })
+
+    it('getMaxPower', ()=> {
+        expect(hashrate.getMaxPower([0, 10, 100])).toEqual(0);
+        expect(hashrate.getMaxPower([1000, 2000, 10000000])).toEqual(2);
+    })
+
+    it('getPowerStr', ()=> {
+        expect(hashrate.getPowerStr(0)).toEqual('H');
+        expect(hashrate.getPowerStr(2)).toEqual('MH');
+    })
+
+    it('hashrateValueToPower', ()=> {
+        expect(hashrate.hashrateValueToPower(1, 0)).toEqual(1);
+        expect(hashrate.hashrateValueToPower(2000, 1)).toEqual(2);
+    })
+
+    it('hashratePowerStr', ()=> {
+        expect(hashrate.hashratePowerStr(1, 0)).toEqual('1.00 H');
+        expect(hashrate.hashratePowerStr(2000, 1)).toEqual('2.00 KH');
     })
 });
